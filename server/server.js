c3957f05b35699c5e36fa80b77ccf00ed12765eb
prajapati prakash <157948148+prakashprajapati48@users.noreply.github.com>
2026-01-03 13:12:55 +0530

    Enhance static file serving with path module
    Added path module for static file serving and corrected static middleware usage.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from "path";
 import authcontroller from './route/authRoutes.js'
 
 const app = express()
@@ -8,7 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api/auth",authcontroller)
+app.use("/api/auth",authcontroller);
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.get("/",(req,res)=>{
     res.redirect(302,"http://localhost:5173")

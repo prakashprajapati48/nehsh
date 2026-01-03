def69060aa76775a58ac2111abcf8e0e3b726025
prajapati prakash <157948148+prakashprajapati48@users.noreply.github.com>
2026-01-03 15:50:51 +0530

    Update Context_api.jsx

--- src/componets/Context/Context_api.jsx
+++ src/componets/Context/Context_api.jsx
@@ -9,18 +9,18 @@ export const Cartprovider = ({ children }) => {
     const [wishList, setWishlist] = useState([]);
 
     const addItem = (item) => {
-        let exist = addedItems.some(addit => addit.productId === item.productId)
+        let exist = addedItems.some(addit => addit.productid === item.productid)
         if (!exist) {
             setAddedItems((prev) => [...prev, item]);
         }
     };
 
     const removeItem = (id) => {
-        setAddedItems((prev) => prev.filter((item) => item.productId !== id));
+        setAddedItems((prev) => prev.filter((item) => item.productid !== id));
     };
 
     const wishlists = (items) => {
-        let exist = wishList.some(check => check.productId === items.productId);
+        let exist = wishList.some(check => check.productid === items.productid);
 
         if (!exist) {
             setWishlist((prev) => [...prev, items]);
@@ -28,7 +28,7 @@ export const Cartprovider = ({ children }) => {
     }
 
     const wishlistRemove = (itemId) => {
-        setWishlist((prev) => prev.filter((item) => item.productId !== itemId))
+        setWishlist((prev) => prev.filter((item) => item.productid !== itemId))
     }
 
     return (

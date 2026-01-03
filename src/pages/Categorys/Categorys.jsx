c8b19f5da543c8caa70ff87d2b7cd43b8daaacee
prajapati prakash <157948148+prakashprajapati48@users.noreply.github.com>
2026-01-03 14:02:22 +0530

    Fix product ID reference in button click handler

--- src/pages/Categorys/Categorys.jsx
+++ src/pages/Categorys/Categorys.jsx
@@ -112,7 +112,7 @@ const Categorys = () => {
                                     {/* <p className="itemDesc">{items.productDesc}</p> */}
                                     <p className="itemPrice">₹{items.productPrice}</p>
                                     <button
-                                        onClick={() => {addedItems.some((itemsbtn) => itemsbtn.productId === items.productid) ? pageredirecter("cart") : handleAdd(items),console.log(`product id: ${addedItems.some((itemsbtn) => itemsbtn.productId === items.productId)}`)}}
+                                        onClick={() => {addedItems.some((itemsbtn) => itemsbtn.productId === items.productid) ? pageredirecter("cart") : handleAdd(items),console.log(`product id: ${addedItems.some((itemsbtn) => itemsbtn.productId === items.productid)}`)}}
                                         id="add_cart_btn">
                                         {addedItems.some((add_items) => add_items.productId === items.productId)
                                             ? "View Cart"

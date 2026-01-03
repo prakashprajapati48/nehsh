017a7d4dd226bb2203fa6f4abd50aa9d1090de19
prajapati prakash <157948148+prakashprajapati48@users.noreply.github.com>
2026-01-03 16:02:00 +0530

    Update Wishlist.jsx

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -37,13 +37,13 @@ const Wishlist = () => {
                   {wishList.map((item, idx) => (
                     <tr key={idx}>
                       <td><img src={item.file} alt="" className="wishlist_img" /></td>
-                      <td className="wishlistItemname">{item.productName}</td>
+                      <td className="wishlistItemname">{item.productname}</td>
                       <td className="wishlistItemstock">In Stock</td>
-                      <td>₹{item.productPrice} <span style={{ textDecoration: "line-through" }}>₹{item.originalPrice}</span></td>
+                      <td>₹{item.productprice} <span style={{ textDecoration: "line-through" }}>₹{item.originalPrice}</span></td>
                       <td>
                         <button>Add To Cart</button>
                       </td>
-                      <td onClick={() => wishlistRemove(item.productId)} className="wishlist_remove"><X /> </td>
+                      <td onClick={() => wishlistRemove(item.productid)} className="wishlist_remove"><X /> </td>
                     </tr>
                   ))}
                 </tbody>
@@ -55,12 +55,12 @@ const Wishlist = () => {
                   <div className="wishlist_mb" key={idx}>
                     <div key={idx} className="wishlistItemImg">
                       <img src={item.file} alt="" className="wishlist_img" />
-                      <span className="removebtn" onClick={() => setIconrmshow(iconrmShow === item.productId ? null : item.productId)}><EllipsisVertical fill='black' /> </span>
-                      <span className={`removeitem ${iconrmShow === item.productId ? "removeitemshow" : "removeitemhide"}`} onClick={() => wishlistRemove(item.productId)} key={idx}>Remove</span>
+                      <span className="removebtn" onClick={() => setIconrmshow(iconrmShow === item.productid ? null : item.productid)}><EllipsisVertical fill='black' /> </span>
+                      <span className={`removeitem ${iconrmShow === item.productid ? "removeitemshow" : "removeitemhide"}`} onClick={() => wishlistRemove(item.productid)} key={idx}>Remove</span>
                     </div>
                     <div className="wishlist_details">
-                      <p className="wishlistItemname">{item.productName}</p>
-                      <p className="wishlistprice">₹{item.productPrice} {item.originalPrice !== null ? <span style={{ textDecoration: "line-through" }}>₹{item.originalPrice}</span> : ""}</p>
+                      <p className="wishlistItemname">{item.productname}</p>
+                      <p className="wishlistprice">₹{item.productprice} {item.originalPrice !== null ? <span style={{ textDecoration: "line-through" }}>₹{item.originalPrice}</span> : ""}</p>
                       <p className="wishlistReviews"><span><Star fill='yellow' stroke='yellow' /> </span>{item.rating}</p>
                       <p className="wishlistItemstock">In Stock</p>
                       <button>Add To Cart</button>

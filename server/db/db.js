dcf84d8098511a0184e0ffcb59e32e2214a5ae6d
Prajapati Prakash <157948148+prakashprajapati48@users.noreply.github.com>
2026-04-28 11:46:19 +0530

    Add SSL configuration to database connection

--- server/db/db.js
+++ server/db/db.js
@@ -12,6 +12,7 @@ import pg from "pg";
 const { Pool } = pg;
 export const connection = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 if (connection) {
